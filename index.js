46130005ab21d9cc4fe577377739f97313c7ccbd
Hope Magat <46736323+jezraelhope@users.noreply.github.com>
2022-12-16 16:07:20 -0600

    initial build part 2

---
+++ index.js
@@ -0,0 +1,41 @@
+const contentContainer = document.getElementById("randomize");
+
+//fetch genre list
+
+const temporaryGenre = [
+  "romance",
+  "comedy",
+  "seinen",
+  "isekai",
+  "mecha",
+  "sports",
+  "psychological",
+  "horror",
+  "adventure",
+  "scifi",
+  "slice-of-life",
+  "shoujo",
+  "shonen",
+];
+
+//generate 4 random genre
+const getRandom4Nums = [];
+while (getRandom4Nums.length < 4) {
+  var r = Math.floor(Math.random() * temporaryGenre.length) + 0;
+  if (getRandom4Nums.indexOf(r) === -1) getRandom4Nums.push(r);
+}
+const random4Genre = getRandom4Nums.map((num) => {
+  return temporaryGenre[num];
+});
+console.log(random4Genre);
+
+//generate components based on genre
+
+const genreComponent = random4Genre.map((genre) => {
+  return `<div class = "${genre}">
+        <h3>${genre}</h3>
+        <div class="anime-list-by-genre"></div> 
+    </>`;
+});
+
+contentContainer.innerHTML = genreComponent;
