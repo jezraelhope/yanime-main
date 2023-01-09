053a5b59e59fb2241493758c55e058881fef3ded
Hope Magat <46736323+jezraelhope@users.noreply.github.com>
2023-01-09 17:38:13 -0600

    fetch anime list from main site api

--- index.js
+++ index.js
@@ -1,21 +1,37 @@
+//get anime data
+let animeList = [];
+const fetchAnime = async () => {
+  const response = await fetch("https://yanime.onrender.com/anime/admin", {
+    method: "GET",
+    mode: "cors",
+    headers: {
+      "Content-Type": "application/json",
+    },
+  })
+    .then((res) => res.json())
+    .then((data) => data);
+};
+fetchAnime();
+//get elements from html
+
 const contentContainer = document.getElementById("randomize");
 
 //fetch genre list
 
 const temporaryGenre = [
-  "romance",
-  "comedy",
-  "seinen",
-  "isekai",
-  "mecha",
-  "sports",
-  "psychological",
-  "horror",
-  "adventure",
-  "scifi",
-  "slice-of-life",
-  "shoujo",
-  "shonen",
+  "Romance",
+  "Comedy",
+  "Seinen",
+  "Isekai",
+  "Mecha",
+  "Sports",
+  "Psychological",
+  "Horror",
+  "Adventure",
+  "Scifi",
+  "Slice-of-life",
+  "Shoujo",
+  "Shonen",
 ];
 
 //generate 4 random genre
@@ -27,13 +43,12 @@ while (getRandom4Nums.length < 4) {
 const random4Genre = getRandom4Nums.map((num) => {
   return temporaryGenre[num];
 });
-console.log(random4Genre);
 
 //generate components based on genre
 
 const genreComponent = random4Genre.map((genre) => {
   return `<div class = "${genre}">
-        <h3>${genre}</h3>
+        <h3>Top Anime in ${genre}</h3>
         <div class="anime-list-by-genre"></div> 
     </>`;
 });
